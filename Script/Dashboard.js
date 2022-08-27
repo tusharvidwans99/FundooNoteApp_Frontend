9006592cc3230ed1f56e185a191d7aa098c6de7f
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-08-27 11:07:37 +0530

    [Tushar Vidwans] Added functionality for Archive/Unarchive notes, Trash/Untrash and permanently delete note

--- Script/Dashboard.js
+++ Script/Dashboard.js
@@ -19,7 +19,7 @@ var noteArray;
 
 getAllNotes();
 
-function getAllNotes(){
+function getAllNotes(data=''){
     let token = localStorage.getItem('token');
 
     $.ajax({
@@ -32,7 +32,17 @@ function getAllNotes(){
         success: function(result){
             console.log(result.data);
             noteArray = result.data;
-            displayAllNotes(noteArray)
+            noteArray.reverse();
+            if(data == displayArchiveNotes){
+                data.click();
+            }
+            else if(data == displayTrashNotes){
+                data.click();
+            }
+            else{
+                displaynotes.click();
+            }
+            
         },
         error: function(error){
             console.log(error);
@@ -40,13 +50,13 @@ function getAllNotes(){
     })
 }
 
-document.getElementById('notes').addEventListener('click', (notes)=>{
-    console.log("Notes : ",notes.target);
-    getArray = noteArray.filter((filter)=>{
-        return filter.archive ==false && filter.trash==false;
-    })
-    console.log("Notes : ", getArray);
-})
+// document.getElementById('notes').addEventListener('click', (notes)=>{
+//     console.log("Notes : ",notes.target);
+//     getArray = noteArray.filter((filter)=>{
+//         return filter.archive ==false && filter.trash==false;
+//     })
+//     console.log("Notes : ", getArray);
+// })
 
 function displayNotes(noteArray){
     console.log("Notes list : ", noteArray);   
@@ -57,25 +67,26 @@ function displayNotes(noteArray){
 function CreateNoteOpen(){
     document.getElementById("form").style.height="165px";
     document.getElementById("pin").style.visibility="visible";
+    document.getElementById("title").placeholder = "Title";
 }
 
 function CreateNoteClose(){
     document.getElementById("form").style.height="55px";
     document.getElementById("pin").style.visibility="hidden";
+    document.getElementById("title").placeholder = "Take a note...";
 }
 
-
+//API call for creating note
 function createNote(){
     let token = localStorage.getItem('token');
 
     let title = document.getElementById("title");
     let description = document.getElementById("description");
-    let Color = 'yellow';
+    
 
     let noteData = {
         title:title.value,
-        description:description.value,
-        color:Color
+        description:description.value
     }
     console.log(noteData);
 
@@ -106,11 +117,12 @@ function resetCreateNote(){
 
 //Display Notes
 
-const displaytnotes=document.querySelector('.notes');
+const displaynotes=document.querySelector('.notes');
 const displayArchiveNotes = document.querySelector('.archive')
 const displayTrashNotes = document.querySelector('.bin')
 
-displaytnotes.addEventListener('click',()=>{
+displaynotes.addEventListener('click',()=>{
+    // getAllNotes();
     notes=noteArray.filter((x)=>{
         return x.trash==false && x.archive==false;
     });
@@ -118,20 +130,20 @@ displaytnotes.addEventListener('click',()=>{
     displayAllNotes(notes);
 })
 
-displayArchiveNotes.addEventListener('click', (notes) => {
+displayArchiveNotes.addEventListener('click', () => {
     notes=noteArray.filter((x)=>{
         return x.trash===false && x.archive===true;
     });
     console.log(notes);
-    displayAllNotes(notes);
+    displayArchivedNotes(notes);
 })
 
-displayTrashNotes.addEventListener('click', (notes) => {
+displayTrashNotes.addEventListener('click', () => {
     notes=noteArray.filter((x)=>{
         return x.trash===true && x.archive===false;
     });
     console.log(notes);
-    displayAllNotes(notes);
+    displayTrashedNotes(notes);
 })
 
 
@@ -146,23 +158,74 @@ function displayAllNotes(Notesdata){
         </div>
         <div class="card-footer">
             <button>    
-                    <img src="../Assets/NotesCreation/remind.svg" />
+                    <img src="../Assets/NotesCreation/remind.svg"/>
             </button>
             <button>
                     <img src="../Assets/NotesCreation/person.svg" />
             </button>
-            <button>
+            <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">
                     <img src="../Assets/NotesCreation/color.svg" />
-            </button>
+                </button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a>red</a>
+                    <a>blue</a>
+                    <a>green</a>
+                </div>
+            </div>
+        
             <button>
                     <img src="../Assets/NotesCreation/image.svg" />
             </button>
-            <button>
+            <button id='card-footer-archive' onclick="ArchiveNote(${note.noteID})">
                     <img src="../Assets/NotesCreation/archive.svg" />
             </button>
-            <button>
-                    <img src="../Assets/NotesCreation/more.svg" />
+
+            <button id='card-footer-more' onclick="TrashNote(${note.noteID})">
+                    <img src="../Assets/Dashboard/trash.svg">
             </button>
+               
+        </div>
+    </div>
+   `
+   ).join(' ');
+};
+
+
+function displayArchivedNotes(Notesdata){
+    console.log(Notesdata);
+   document.getElementById('Notes').innerHTML=Notesdata.map((note)=>
+   `<div class="display-div">
+        <div class="display-content">
+            <p class="ti">${note.title}</p>
+            <P class="de">${note.description}</P>
+        </div>
+        <div class="card-footer">
+            
+            <button>
+                    <img src="../Assets/NotesCreation/person.svg" />
+            </button>
+            <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">
+                    <img src="../Assets/NotesCreation/color.svg" />
+                </button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a>red</a>
+                    <a>blue</a>
+                    <a>green</a>
+                </div>
+            </div>
+            <button>
+                    <img src="../Assets/NotesCreation/image.svg" />
+            </button>
+            <button id='card-footer-archive' onclick="UnArchiveNote(${note.noteID})">
+                    <img style="transform: rotate(180deg);" src="../Assets/NotesCreation/archive.svg" />
+            </button>
+
+            <button id='card-footer-more' onclick="TrashNote(${note.noteID})">
+                    <img src="../Assets/Dashboard/trash.svg">
+            </button>
+               
         </div>
     </div>
    `
@@ -171,11 +234,238 @@ function displayAllNotes(Notesdata){
 
 
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+
+function displayTrashedNotes(Notesdata){
+    console.log(Notesdata);
+   document.getElementById('Notes').innerHTML=Notesdata.map((note)=>
+   `<div class="display-div">
+        <div class="display-content">
+            <p class="ti">${note.title}</p>
+            <P class="de">${note.description}</P>
+        </div>
+        <div class="card-footer">
+
+            <button id='card-footer-more' onclick="Delete(${note.noteID})">
+                    <img src="../Assets/Dashboard/deleteforever.svg">
+            </button>
+
+            <button id='card-footer-more' onclick="UnTrashNote(${note.noteID})">
+                    <img src="../Assets/Dashboard/Restore.svg">
+            </button>
+               
+        </div>
+    </div>
+   `
+   ).join(' ');
+};
+
+
+
+// document.getElementById('Notes').addEventListener('click',(e)=>{
+//     console.log(e.target.id);
+// })
+
+
 // Refresh page
 
 
 const Refresh = document.getElementById('refresh');
 
 Refresh.addEventListener('click', ()=>{
-    displayAllNotes(noteArray);
+    getAllNotes();
 })
+
+
+//Archive Note API
+function ArchiveNote(noteid){
+    let token = localStorage.getItem('token');
+
+    
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Archive/${noteid}`,
+        type:'PUT',
+        // data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes();
+            // displaynotes.click();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+// Unarchive notes
+
+function UnArchiveNote(noteid){
+    let token = localStorage.getItem('token');
+
+    
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Archive/${noteid}`,
+        type:'PUT',
+        // data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes(displayArchiveNotes);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+//TrashNote API
+
+function TrashNote(noteid){
+    let token = localStorage.getItem('token');
+
+    
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Trash/${noteid}`,
+        type:'PUT',
+        // data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes();
+            // displaynotes.click();
+            // resetCreateNote();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+
+function UnTrashNote(noteid){
+    let token = localStorage.getItem('token');
+
+    
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Trash/${noteid}`,
+        type:'PUT',
+        // data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes(displayTrashNotes);
+            // displaynotes.click();
+            // resetCreateNote();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+function Delete(noteid){
+    let token = localStorage.getItem('token');
+
+    
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Delete/${noteid}`,
+        type:'DELETE',
+        // data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes(displayTrashNotes);
+            // displayTrashNotes.click();
+            // displaynotes.click();
+            // resetCreateNote();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+// Color change API
+
+function ChangeColor(noteid, clr){
+    let token = localStorage.getItem('token');
+
+    data = {
+        NoteID:noteid,
+        color:clr
+    }
+    // console.log(noteData);
+
+    $.ajax({
+        url:`https://localhost:44306/api/Notes/Color`,
+        type:'PUT',
+        data:JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' +token
+        },
+        success: function(result){
+            console.log(result);
+            getAllNotes(displayTrashNotes);
+            // displayTrashNotes.click();
+            // displaynotes.click();
+            // resetCreateNote();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+
+
+
+  
